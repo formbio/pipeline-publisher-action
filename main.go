33b022efffb141f9dbffef7465ac75e67e854aed
darrenmcc <darren.rmc@gmail.com>
2022-04-25 10:36:28 -0400

    Init

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+var (
+	ctx   = context.Background()
+	creds = flag.String("creds", "", "")
+)
+
+func main() {
+	jwt, err := google.JWTConfigFromJSON([]byte(*creds))
+	if err != nil {
+		log.Fatalf("unable to read credentials: %s", err)
+	}
+
+	_ = jwt
+
+	gcs, err := storage.NewClient(ctx, option.WithCredentialsJSON([]byte(*creds)))
+	if err != nil {
+		log.Fatalf("unable to init gcs client: %s", err)
+	}
+
+	bucket := gcs.Bucket("bantha-pipelines")
+
+	it := bucket.Objects(ctx, nil)
+	for {
+		attrs, err := it.Next()
+		if err != nil {
+			if err == iterator.Done {
+				break
+			}
+			log.Fatalf("Bucket(%q).Objects: %v", bucket, err)
+		}
+		log.Println(attrs.Name)
+	}
+}
